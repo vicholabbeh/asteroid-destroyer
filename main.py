5ea22e35d9a82812bcb76abb9822a5e28e35b727
vicholabbeh <vicholabbe@gmail.com>
2026-06-24 18:32:33 -0400

    added CircleShape class

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0.0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -17,7 +19,8 @@ def main():
             if event.type == pygame.QUIT:
                 return
             pygame.display.flip()
-
+        dt = clock.tick(60) / 1000
+            
 
 if __name__ == "__main__":
     main()
